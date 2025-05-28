cb4931619f982d14edeb543d8d3941a42b0b0846
daphniele <101435384+daphniele@users.noreply.github.com>
2025-05-28 19:22:42 -0300

    Inserção na tabela de clientes ok, impressão das operações ok

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def redo():
   
     begins_encontrados=[]
     ends_encontrados=[]
-    transacoes_nao_finalizadas = []
 
     i = 0
     while i < len(log):
@@ -30,27 +29,47 @@ def redo():
         
         i = i+1
 
-        #compara número das transações para saber quais finalizaram
+    #compara número das transações para saber quais finalizaram
 
-        transacoes_comitadas = list(set(begins_encontrados) & set(ends_encontrados))
-        transacoes_nao_finalizadas = list(set(begins_encontrados) - set(ends_encontrados))
-
-        #refaz as transações que finalizaram
-
-        # busca na tabela de log a transação
+    transacoes_comitadas = list(set(begins_encontrados) & set(ends_encontrados))
+    transacoes_nao_finalizadas = list(set(begins_encontrados) - set(ends_encontrados))
 
 
-
-  
+    # imprime as transações:
         
+    print("Transações que realizaram REDO:", ', '.join(str(x) for x in transacoes_comitadas))
+    print("Transações que NÃO realizaram REDO:", ', '.join(str(x) for x in transacoes_nao_finalizadas))
 
+    #refaz as transações que finalizaram
+    i = 0
+    while i < len(log):
+        linha = log[i]
+        id_operacao, operacao, id_cliente, nome, saldo = linha
+
+        if id_operacao in transacoes_comitadas:
+
+            if operacao == 'I': 
+                cursor.execute(
+                    "INSERT INTO clientes_em_memoria (id, nome, saldo) VALUES (%s, %s, %s)",
+                    (id_cliente, nome, saldo)
+                )
+
+            elif operacao == 'U': 
+                cursor.execute(
+                    "UPDATE clientes_em_memoria SET nome = %s, saldo = %s WHERE id = %s",
+                    (nome, saldo, id_cliente)
+                )
+
+            elif operacao == 'D':
+                cursor.execute(
+                    "DELETE FROM clientes_em_memoria WHERE id = %s",
+                    (id_cliente,)
+                )
+
+        i += 1
    
-        
-        
-      
-    
-    
-  
+    #executa as ações finaliza a conexão com o banco
+    conexao.commit()
     cursor.close()
     conexao.close()
 
